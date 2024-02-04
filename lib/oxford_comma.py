1b5bd5998e612059bfd3230c6d9af81a53f31b20
patrickonesiah <patrickonesiah@gmail.com>
2024-02-05 00:35:38 +1100

    Lab completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return ", ".join(items)
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) == 3:
+        return ", and ".join([", ".join(items[:-1]),items[-1]])
+    else:
+        return ", and ".join([", ".join(items[:-1]),items[-1]])
+
